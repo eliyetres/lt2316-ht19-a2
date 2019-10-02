3dacb936dd929682834b3b63324c9bb01b0935b4
Elin Hagman <eliyetres@hotmail.com>
2019-10-02 23:26:26 +0200

    finished preprocessing, tokenization and adding boundaries to sentences

--- read_data.py
+++ read_data.py
@@ -1,13 +1,17 @@
 import os
 import re
-import nltk.tokenize.punkt
+import nltk.data
+import nltk.tokenize.punkt 
 import xml.etree.ElementTree as ET
 from config import data_dir
 
 print(data_dir)
 
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
 def process_data_tags(tag, dirname):
-    text = ""
+    """ Takes a speech tag as string and a directory name. Processes all XML files in the directory """
+    sentences = []
     for filename in os.listdir(dirname):
         if not filename.endswith('.xml'): continue
         fullname = os.path.join(dirname, filename)
@@ -15,28 +19,26 @@ def process_data_tags(tag, dirname):
         root = tree.getroot()
         speech_tags = root.findall("speech")
         speakers = []
-        for item in speech_tags:           
+        for item in speech_tags:
+            #print(ET.dump(item)) # use this to print roots           
             try:  # check if there is no name for speaker
                 item.attrib['nospeaker']
-                boundary = "[CHANGE]" # should there be a different tag if we don't know if they change?
+                boundary = "[SAME]"
             except:                
-                while len(speakers) < 2:    
+                while len(speakers) < 2: # check if two speakers are the same    
                     speaker = item.attrib['speakername']
                     speakers.append(speaker)
-                    #print(speaker)
-                #print(speakers)
                 if speakers[0] == speakers[1]:
                     boundary = "[SAME]"
                 else:
-                    boundary = "[CHANGE]"
-        
-            speakers = []
-            notags = ET.tostring(item, encoding='unicode', method='text')  
-            notags = re.sub(r'(\s{2,})', '', notags)
-            text = text + notags + " "+boundary+" " 
-            #print(ET.dump(item))
-            
-    print(text)
-    return text
+                    boundary = "[CHANGE]"        
+            speakers = [] # reset check            
+            notags = ET.tostring(item, encoding='unicode', method='text') 
+            notags = re.sub(r'(\s{2,})', ' ', notags)
+            punkt = sent_detector.tokenize(notags)           
+            for s in punkt:
+                sentences.append(s+ " "+boundary) # add boundaries to every sentence 
+    print(sentences)
+    return sentences
 
 process_data_tags("speech", data_dir)
