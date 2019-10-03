4549bbd5a911f0ad8c4bed7c280c303c335a2578
azfar-imtiaz <azfar.imtiaz@live.com>
2019-10-03 17:29:55 +0200

    Modify data to write sentence-boundary to csv to accomodate to new structure

--- read_data.py
+++ read_data.py
@@ -12,47 +12,51 @@ def process_data_tags(tag, dirname):
     """ Takes a speech tag as string and a directory name. Processes all XML files in the directory """
     sentences = []
     for filename in os.listdir(dirname):
-        if not filename.endswith('.xml'): continue
-    for index, filename in enumerate(os.listdir(dirname)):
-        if index > 20:
-            break
-        print("Currently processing: {}".format(filename))
         if not filename.endswith('.xml'):
             continue
-        fullname = os.path.join(dirname,filename)
+    for index, filename in enumerate(os.listdir(dirname)):
+        print("Currently processing: {}".format(filename))
+        # if index > 20:
+        #     break
+        if not filename.endswith('.xml'):
+            continue
+        fullname = os.path.join(dirname, filename)
         tree = ET.parse(fullname)
         root = tree.getroot()
         speech_tags = root.findall("speech")
         speakers = []
-        for item in speech_tags:            
+        for item in speech_tags:
             if len(speakers) > 2:
                 speakers.pop(0)
             # print(ET.dump(item)) # use this to print roots
             try:  # check if there is no name for speaker
                 item.attrib['nospeaker']
-                boundary = "[SAME]"               
-                speakers.append("noname")  #add to speaker list 
+                boundary = "[SAME]"
+                speakers.append("noname")  # add to speaker list
             except:
                 speaker = item.attrib['speakername']
                 speakers.append(speaker)
-                if len(speakers) < 2:  # first speaker                               
+                if len(speakers) < 2:  # first speaker
                     boundary = "[SAME]"
-                else: # thereafter
+                else:  # thereafter
                     if speakers[0] == speakers[1]:
                         boundary = "[SAME]"
-                    else:                      
+                    else:
                         boundary = "[CHANGE]"
 
             notags = ET.tostring(item, encoding='unicode', method='text')
             notags = re.sub(r'(\s{2,})', ' ', notags)
             punkt = sent_detector.tokenize(notags)
-            for s in punkt:
+            for index, s in enumerate(punkt):
                 sentences.append(s.strip())
+                if index == len(punkt) - 1:
+                    break
                 sentences.append("[SAME]")
-            sentences.pop(-1) # idk how to do this nicer, tried for s in punkt[:-1]
+            # idk how to do this nicer, tried for s in punkt[:-1]
+            # sentences.pop(-1)
             sentences.append(boundary)
-            
-    #print(sentences)
+
+    # remove the last boundary because the very last sentence should not have a boundary after it
     sentences.pop(-1)
     return sentences
 
@@ -62,18 +66,11 @@ def write_sents_to_csv(sentences, filename):
         writer = csv.writer(wfile)
         # write header
         writer.writerow(["Sentence 1", "Sentence 2", "Boundary"])
-        for index in range(len(sentences)):
+        for index in range(0, len(sentences), 2):
             try:
                 sent1 = sentences[index]
-                sent2 = sentences[index + 1]
-                rg_boundary = r'\[(SAME|CHANGE)\]'
-                # get the boundary from the end of the first sentence. We get group(1) so that we don't get the 
-                # surrounding square brackets
-                boundary = re.search(
-                    rg_boundary, sent1[-10:]).group(1)
-                # remove the [SAME] or [CHANGE] from the end of the sentence
-                sent1 = re.sub(rg_boundary, '', sent1).strip()
-                sent2 = re.sub(rg_boundary, '', sent2).strip()
+                boundary = sentences[index + 1][1:][:-1]
+                sent2 = sentences[index + 2]
                 writer.writerow([sent1, sent2, boundary])
             except IndexError:
                 pass
@@ -82,4 +79,5 @@ def write_sents_to_csv(sentences, filename):
 print(config.data_dir)
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 sentences = process_data_tags("speech", config.data_dir)
-#write_sents_to_csv(sentences, config.CSV_FILENAME)
+print("Writing data to scv...")
+write_sents_to_csv(sentences, config.CSV_FILENAME)
