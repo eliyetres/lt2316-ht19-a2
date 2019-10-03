67564c73b8af0e7ee39d9c1e8f5ea6eea51f49bf
Elin Hagman <eliyetres@hotmail.com>
2019-10-03 16:13:24 +0200

    changed processing the files, now properly changes boundaries

--- read_data.py
+++ read_data.py
@@ -11,39 +11,49 @@ import config
 def process_data_tags(tag, dirname):
     """ Takes a speech tag as string and a directory name. Processes all XML files in the directory """
     sentences = []
+    for filename in os.listdir(dirname):
+        if not filename.endswith('.xml'): continue
     for index, filename in enumerate(os.listdir(dirname)):
         if index > 20:
             break
         print("Currently processing: {}".format(filename))
         if not filename.endswith('.xml'):
             continue
-        fullname = os.path.join(dirname, filename)
+        fullname = os.path.join(dirname,filename)
         tree = ET.parse(fullname)
         root = tree.getroot()
         speech_tags = root.findall("speech")
         speakers = []
-        for item in speech_tags:
+        for item in speech_tags:            
+            if len(speakers) > 2:
+                speakers.pop(0)
             # print(ET.dump(item)) # use this to print roots
             try:  # check if there is no name for speaker
                 item.attrib['nospeaker']
-                boundary = "[SAME]"
+                boundary = "[SAME]"               
+                speakers.append("noname")  #add to speaker list 
             except:
-                while len(speakers) < 2:  # check if two speakers are the same
-                    speaker = item.attrib['speakername']
-                    speakers.append(speaker)
-                if speakers[0] == speakers[1]:
+                speaker = item.attrib['speakername']
+                speakers.append(speaker)
+                if len(speakers) < 2:  # first speaker                               
                     boundary = "[SAME]"
-                else:
-                    print("I get here!")
-                    boundary = "[CHANGE]"
-            speakers = []  # reset check
+                else: # thereafter
+                    if speakers[0] == speakers[1]:
+                        boundary = "[SAME]"
+                    else:                      
+                        boundary = "[CHANGE]"
+
             notags = ET.tostring(item, encoding='unicode', method='text')
             notags = re.sub(r'(\s{2,})', ' ', notags)
             punkt = sent_detector.tokenize(notags)
             for s in punkt:
-                # add boundaries to every sentence
-                sentences.append(s.strip() + " " + boundary)
-    # print(sentences)
+                sentences.append(s.strip())
+                sentences.append("[SAME]")
+            sentences.pop(-1) # idk how to do this nicer, tried for s in punkt[:-1]
+            sentences.append(boundary)
+            
+    #print(sentences)
+    sentences.pop(-1)
     return sentences
 
 
@@ -72,4 +82,4 @@ def write_sents_to_csv(sentences, filename):
 print(config.data_dir)
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 sentences = process_data_tags("speech", config.data_dir)
-write_sents_to_csv(sentences, config.CSV_FILENAME)
+#write_sents_to_csv(sentences, config.CSV_FILENAME)
