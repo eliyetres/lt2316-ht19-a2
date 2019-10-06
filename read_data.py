41ab756f4bd272d3954bc7ce4b0ccb47d50343f1
Sandra Derbring <sandra.derbring@gmail.com>
2019-10-06 19:42:21 +0200

    added post processing functionality

--- read_data.py
+++ read_data.py
@@ -10,13 +10,14 @@ import config
 
 def process_data_tags(tag, dirname):
     """ Takes a speech tag as string and a directory name. Processes all XML files in the directory """
+
     words = []
     boundaries = []
     for filename in os.listdir(dirname):
         if not filename.endswith('.xml'):
             continue
     for index, filename in enumerate(os.listdir(dirname)):
-        print("Currently processing: {}".format(filename))
+        #print("Currently processing: {}".format(filename))
         # if index > 20:
         #     break
         if not filename.endswith('.xml'):
@@ -58,6 +59,72 @@ def process_data_tags(tag, dirname):
     boundaries.pop(-1)
     return words, boundaries
 
+
+def post_process(words, bound):
+    
+    new_words = []
+    new_bound = []
+    
+    #-1 because the last boundary is removed
+    for i in range(0, len(words)-1):
+        #Loops with two sentences at a time to see if they
+        #contain the given expressions
+        if(check_expression(words[i], words[i+1])):
+            new_w = words[i]+' '+words[i+1]
+            new_b = bound[i+1]
+
+            #Print lines found by an expression
+            #print_check(i, words[i], bound[i], i+1, words[i+1], bound[i+1], new_w, new_b)
+            
+            #Replace next index in sentence list with the concatenation of current and next
+            #Don't need to replace tag since we want the last value and it's already at this index
+            words[i+1] = new_w
+            
+        else:
+            #Write complete sentences and their boundaries to the lists
+            new_words.append(words[i])
+            new_bound.append(bound[i])
+
+    #Write last to new list since it's disregarded from the loop
+    new_words.append(words[len(words)-1])
+
+    return new_words, new_bound
+    
+def check_expression(sent1, sent2):
+
+    abbr = ['G.P.', 'N.H.S.', 'cent.', 'Ltd.', 'e.g.', 'i.e.', 'etc.']
+    comments = ['rose—', 'indicated assent.', 'rose —', 'indicated dissent.']
+    number_exps = ['Nos.', 'No.']
+    honorary = ['hon.', 'Hon.']
+    dashes = ['-', '—', ']']
+
+    sent1_last = sent1.split()[-1]
+
+    # Sentence pair where the words in comments and dashes are left as they are.
+    # Second pair entences that start with lowercase but don't fall into the following conditions are also left as they are
+    # even though some misspelt sentences like "percent.", "per. cent.", and "Which?" will not be captured
+
+    # Where first sentence ends with 'hon.' or 'Hon''
+    if sent1_last in honorary: #sent2 does not need to start with lowercase
+        return True
+    # Where first sentence ends with No. or Nos. and second start with a digit
+    elif sent1_last in number_exps and len(sent1) > 3 and sent2[:1].isdigit(): 
+        return True
+    # Where first sentence ends with an abbreviation in the list and second is lowercase
+    elif sent1_last in abbr and sent2[:1].islower(): 
+        return True
+    # Where the first sentence ends with a quotation mark and second is lowercase
+    elif sent1.endswith('\"') and sent2[:1].islower(): 
+        return True
+        
+
+def print_check(i1, sent1, bound1, i2, sent2, bound2, new_w, new_b):
+    print(i1, sent1, bound1)
+    print(i2, sent2, bound2)
+    print(new_w, new_b)
+    print('\n')
+    
+
 def write_sents_to_csv(sentences, filename):
     with open(filename, 'w', encoding="utf-8") as wfile:
         writer = csv.writer(wfile)
@@ -76,11 +143,27 @@ def write_sents_to_csv(sentences, filename):
 import pandas as pd
 
 
-
+print("Loading data...")
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+print("Processing data tags...")
 words, bound = process_data_tags("speech", config.data_dir)
 
-df = pd.DataFrame(list(zip(words, bound)), columns =['Sentence', "Boundary"]) 
+print("Post processing...")
+words2, bound2 = post_process(words, bound)
+
+#Prints
+print("Lengths of sentence and boundaries lists before post processing:")
+print(len(words))
+print(len(bound))
+
+print("Lengths of sentence and boundaries lists after post processing:")
+print(len(words2))
+print(len(bound2))
+
+#df = pd.DataFrame(list(zip(words, bound)), columns =['Sentence', "Boundary"]) 
+df = pd.DataFrame(list(zip(words2, bound2)), columns =['Sentence', "Boundary"])
+
 print("Writing data to csv...")
 df.to_csv(config.CSV_FILENAME, index=False, encoding='utf-8')
 #write_sents_to_csv(sentences, config.CSV_FILENAME)
