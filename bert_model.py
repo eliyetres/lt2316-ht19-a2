6acb6b30c5af61ca61912cd7649104d6e03d25ed
azfar-imtiaz <azfar.imtiaz@live.com>
2019-10-10 20:03:36 +0200

    Add data testing to bert_model script

--- bert_model.py
+++ bert_model.py
@@ -44,11 +44,56 @@ def create_segment_masks(preprocessed_train_data, max_sent_len):
     return segment_masks
 
 
+def prepare_data(data, tokenizer):
+    preprocessed_data, labels = preprocess_sents_bert_style(data, tokenizer)
+
+    max_sent_len = max(len(a) for a in preprocessed_data)
+    # get numeric representations of tokens
+    input_ids = [tokenizer.convert_tokens_to_ids(
+        x) for x in preprocessed_data]
+    # pad sequences
+    padded_seqs = pad_sequence([torch.LongTensor(x)
+                                for x in input_ids], batch_first=True)
+    # create segment masks for separating two sentences
+    segment_masks = create_segment_masks(
+        preprocessed_data, len(padded_seqs[0]))
+
+    # create attention masks
+    attention_masks = []
+    for seq in padded_seqs:
+        seq_mask = [float(x > 0) for x in seq]
+        attention_masks.append(seq_mask)
+
+    # make everything a tensor
+    tensor_seqs = torch.LongTensor(padded_seqs)
+    tensor_labels = torch.LongTensor(labels)
+    tensor_attention_masks = torch.LongTensor(attention_masks)
+    tensor_segment_masks = torch.LongTensor(attention_masks)
+
+    # batching
+    batch_size = config.BERT_BATCH_SIZE
+    # make an iterator
+    tensor_data = TensorDataset(
+        tensor_seqs, train_segment_masks, train_attention_masks, train_labels)
+    tensor_sampler = RandomSampler(tensor_data)
+    tensor_dataloader = DataLoader(
+        tensor_data, sampler=tensor_sampler, batch_size=batch_size)
+
+    return tensor_dataloader
+
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
 if __name__ == '__main__':
     # train_data is the same thing as the train_data and test_data outputs from preprocess_data, just pickled
     # This helps avoid having to run the preprocess_data script everytime
     print("Loading data and models...")
     train_data = joblib.load('train_data.pkl')
+    test_data = joblib.load('test_data.pkl')
     tokenizer = BertTokenizer.from_pretrained(
         'bert-base-uncased', do_lower_case=True)
     # initialize the model with 2 output classes
@@ -69,40 +114,8 @@ if __name__ == '__main__':
 
     # INPUTS
     print("Preparing data...")
-    preprocessed_train_data, train_labels = preprocess_sents_bert_style(
-        train_data, tokenizer)
-
-    max_sent_len = max(len(a) for a in preprocessed_train_data)
-    # get numeric representations of tokens
-    input_ids = [tokenizer.convert_tokens_to_ids(
-        x) for x in preprocessed_train_data]
-    # pad sequences
-    padded_seqs = pad_sequence([torch.LongTensor(x)
-                                for x in input_ids], batch_first=True)
-    # create segment masks for separating two sentences
-    train_segment_masks = create_segment_masks(
-        preprocessed_train_data, len(padded_seqs[0]))
-
-    # create attention masks
-    train_attention_masks = []
-    for seq in padded_seqs:
-        seq_mask = [float(x > 0) for x in seq]
-        train_attention_masks.append(seq_mask)
-
-    # make everything a tensor
-    train_seqs = torch.LongTensor(padded_seqs)
-    train_labels = torch.LongTensor(train_labels)
-    train_attention_masks = torch.LongTensor(train_attention_masks)
-    train_segment_masks = torch.LongTensor(train_segment_masks)
-
-    # batching
-    batch_size = config.BERT_BATCH_SIZE
-    # make an iterator
-    train_data = TensorDataset(
-        train_seqs, train_segment_masks, train_attention_masks, train_labels)
-    train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(
-        train_data, sampler=train_sampler, batch_size=batch_size)
+    train_dataloader = prepare_data(train_data, tokenizer)
+    test_dataloader = prepare_data(test_data, tokenizer)
 
     # TRAINING
     print("Training the model...")
@@ -134,3 +147,25 @@ if __name__ == '__main__':
             nb_tr_steps += 1
 
         print("Train loss: {}".format(tr_loss / nb_tr_steps))
+
+    print("Evaluating the model...")
+    model.eval()
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+
+    for batch in test_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, segment_masks, attention_masks, labels = batch
+        with torch.no_grad():
+            logits = model(input_ids, token_type_ids=segment_masks,
+                           attention_mask=attention_masks, labels=labels)
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = labels.to('cpu').numpy()
+
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+
+    print("Testing data accuracy: {}".format(eval_accuracy / nb_eval_steps))
