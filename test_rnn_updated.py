2699ee5ce43c4422ad27020cdb6b13769962b25b
Elin Hagman <eliyetres@hotmail.com>
2019-11-19 12:00:52 +0100

    modified the test script for model using attention mechanism

--- test_rnn_updated.py
+++ test_rnn_updated.py
@@ -2,6 +2,7 @@ import joblib
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from nltk.tokenize import word_tokenize
 from torch.optim import Adam
 from torch.utils import data
 
@@ -12,6 +13,8 @@ from utils import (generate_batch_vectors, generate_sent_vector,
                    get_boundary_mapping, get_word_vector, load_model,
                    print_evaluation_score, read_data_from_csv)
 
+use_attention = config.USE_ATTENTION
+
 # CUDA settings
 use_cuda = torch.cuda.is_available()
 device = torch.device(config.DEVICE)
@@ -21,10 +24,29 @@ if __name__ == '__main__':
     predicted = []
     actual = []
 
+    # Load trained models and data
     print("Loading models...")
     model1 = torch.load("model_1.pkl")
     model2 = torch.load("model_2.pkl")
     classifier = torch.load("classifier.pkl")
+
+    print("Loading pre-trained embeddings...")
+    w2v_model = load_model(config.PATH_TO_PRETRAINED_EMBEDDINGS)
+
+    print("Loading training data...")
+    test_data = read_data_from_csv(config.CSV_FILENAME_TEST, train=False)
+
+    print("\tTotal length of test data: {}".format(len(test_data)))
+    print("\tNumber of SAME records: {}".format(len([a for a in test_data if a['boundary'] == '[SAME]'])))
+    print("\tNumber of CHANGE records: {}".format(len([a for a in test_data if a['boundary'] == '[CHANGE]'])))
+
+    print("Creating data generator...")
+    test_set = Dataset(test_data)
+    test_generator = data.DataLoader(
+        dataset=test_set,
+        drop_last=True,
+        batch_size=1,
+        shuffle=False)
 
     # GPU
     classifier = classifier.to(device)
@@ -33,43 +55,48 @@ if __name__ == '__main__':
 
     # Set parameters to eval mode
     model1.eval()
-    model2.eval()
-
-    print("Loading pre-trained embeddings...")
-    w2v_model = load_model(config.PATH_TO_PRETRAINED_EMBEDDINGS)
-
-    print("Loading training data...")
-    train_data = read_data_from_csv(config.CSV_FILENAME_TEST, train=False)
-
-    print("Creating data generator...")
-    test_set = Dataset(train_data)
-    train_generator = data.DataLoader(
-        dataset=test_set,
-        drop_last=True,
-        batch_size=1,
-        shuffle=False)
+    model2.eval()   
 
     print("Evaluating model...")
-    for sent1, sent2, boundary in train_generator:
-        sent1_vectors = generate_batch_vectors(sent1, w2v_model)
-        sent2_vectors = generate_batch_vectors(sent2, w2v_model)
-        boundary = get_boundary_mapping(boundary)
+    for sent1, sent2, boundary in test_generator:
 
+        # If the model was trained using attention
+        if use_attention is True:
+            # when using attention, seq_len for both sentences need to be the same
+            max_sent_len = max(
+                    max([len(word_tokenize(a)) for a in sent1]),
+                    max([len(word_tokenize(a)) for a in sent2]))
+            sent1_vectors = generate_batch_vectors(sent1, w2v_model, max_sent_len=max_sent_len)
+            sent2_vectors = generate_batch_vectors(sent2, w2v_model, max_sent_len=max_sent_len)
+
+
+        # Model without attention
+        if use_attention is False:
+            sent1_vectors = generate_batch_vectors(sent1, w2v_model)
+            sent2_vectors = generate_batch_vectors(sent2, w2v_model)
+
+        boundary = get_boundary_mapping(boundary)
+        # to GPU
         sent1_vectors = sent1_vectors.to(device)
         sent2_vectors = sent2_vectors.to(device)
         boundary = torch.Tensor(boundary).to(device)
-
+        # forward
         output1, hidden1 = model1(sent1_vectors)
         output2, hidden2 = model2(sent2_vectors)
+        # attention does not combine output
+        if use_attention is True:
+            output = classifier(output1, output2)
 
-        hidden1 = hidden1.squeeze(dim=0)
-        hidden2 = hidden2.squeeze(dim=0)
+        if use_attention is False:
+            hidden1 = hidden1.squeeze(dim=0)
+            hidden2 = hidden2.squeeze(dim=0)
+            combined_hidden = torch.cat([hidden1, hidden2], dim=1)
+            output = classifier(combined_hidden)
 
-        combined_hidden = torch.cat([hidden1, hidden2], dim=1)
-
-        output = classifier(combined_hidden)
         output = output.squeeze(dim=1)
 
+        # #################################################
+        # check if it works from here
         #_, predicted_boundary = torch.max(output.data, dim=1)
         _, predicted_boundary = torch.max(output.data, dim=0)
 
